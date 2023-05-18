6105b232354031e71ee8183939a6487b95a3f23a
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-19 00:38:30 +0600

    error Page Added

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -3,17 +3,33 @@ import banner from "../../../public/toy-truck-2.jpg"
 const Banner = () => {
     return (
         <div className="hero min-h-screen w-full mb-12 rounded-xl" style={{ backgroundImage: `url(${banner})` }}>
-            <div className="hero-overlay bg-opacity-40"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md text-white  absolute rounded-xl flex items-center h-full left-16 ml-0 top-28  ">
-                    <div className="space-y-7  pl-20">
-                        <h1 className="mb-5 text-5xl font-bold">Welcome to Toy Truck Zone</h1>
-                        <p className="mb-8 ">A toy truck website is an online platform dedicated to offering a wide range of toy trucks for children and truck enthusiasts alike. </p>
-                        <Link to="/"><button className="btn btn-active btn-secondary mt-10 px-12">All Toy</button></Link>
+            <div className="hero-overlay bg-opacity-40">
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md text-white  absolute rounded-xl flex items-center h-full left-16 ml-0 top-28  ">
+                        <div className="space-y-7  pl-20 ">
+                            <h1 className="mb-5 text-5xl font-bold">Welcome to Toy Truck Zone</h1>
+                            <p className="mb-8 ">A toy truck website is an online platform dedicated to offering a wide range of toy trucks for children and truck enthusiasts alike. </p>
+                            <Link to="/"><button className="btn btn-active btn-secondary mt-10 px-12">All Toy</button></Link>
+                        </div>
                     </div>
+
+                    
                 </div>
             </div>
         </div>
+        // bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]
+    //     <div id="slide1" className="carousel-item relative w-full ">
+    //     <img src={banner} className="w-full rounded-xl" />
+    //     <div className="absolute rounded-xl flex items-center h-full left-0 right-5 top-0 bg-gradient-to-r from-[#ceb8b8] to-[#a39292] ">
+    //         <div className="text-white space-y-7 w-1/2 pl-24">
+    //             <h2 className="text-6xl font-semibold ">Affordable Price For Car Servicing</h2>
+    //             <p className="font-medium">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+    //             <div>
+    //                 <button className="btn btn-warning mr-5">Discover More</button>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
     );
 };
 
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -4,6 +4,7 @@ import img3 from '../../../public/truck-3.png'
 import img4 from '../../../public/truck-4.png'
 import img5 from '../../../public/truck-5.png'
 import img6 from '../../../public/truck-6.png'
+import { Link } from 'react-router-dom'
 const Gallery = () => {
     return (
         <div>
@@ -12,7 +13,22 @@ const Gallery = () => {
             <div className='mt-12 grid grid-cols-1 md:grid-cols-3 gap-5'>
                 <div className="card w-96 bg-base-100 drop-shadow-2xl">
                     <figure className="px-10 pt-10">
-                        <img src={img1} alt="Shoes" className="rounded-xl" />
+                        <img src={img1} alt="Shoes" className="rounded-xl h-60" />
+                    </figure>
+                    <div className="card-body items-center text-center mb-0">
+                        <h2 className="card-title">Toy Truck</h2>
+                        <div className="card-actions">
+                           <Link to = "https://brudertoyshop.com/collections/commercial"> <button className="btn btn-primary">Buy Now</button></Link>
+                        </div>
+                    </div>
+                </div>
+
+                {/* //////////////////// */}
+                {/* ///////////// */}
+
+                <div className="card w-96 bg-base-100 drop-shadow-2xl">
+                    <figure className="px-10 pt-10">
+                        <img src={img2} alt="Shoes" className="rounded-xl h-60" />
                     </figure>
                     <div className="card-body items-center text-center mb-0">
                         <h2 className="card-title">Toy Truck</h2>
@@ -27,22 +43,7 @@ const Gallery = () => {
 
                 <div className="card w-96 bg-base-100 drop-shadow-2xl">
                     <figure className="px-10 pt-10">
-                        <img src={img2} alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center mb-0">
-                        <h2 className="card-title">Toy Truck</h2>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-
-                {/* //////////////////// */}
-                {/* ///////////// */}
-
-                <div className="card w-96 bg-base-100 drop-shadow-2xl">
-                    <figure className="px-10 pt-10">
-                        <img src={img3} alt="Shoes" className="rounded-xl" />
+                        <img src={img3} alt="Shoes" className="rounded-xl  h-60" />
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Toy Truck</h2>
@@ -57,7 +58,7 @@ const Gallery = () => {
 
                 <div className="card w-96 bg-base-100 drop-shadow-2xl">
                     <figure className="px-10 pt-10">
-                        <img src={img4} alt="Shoes" className="rounded-xl" />
+                        <img src={img4} alt="Shoes" className="rounded-xl  h-60" />
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Toy Truck</h2>
@@ -70,9 +71,9 @@ const Gallery = () => {
                 {/* //////////////////// */}
                 {/* ///////////// */}
 
-                <div className="card w-full bg-base-100 drop-shadow-2xl">
+                <div className="card w-96 bg-base-100 drop-shadow-2xl">
                     <figure className="px-10 pt-10">
-                        <img src={img5} alt="Shoes" className="rounded-xl" />
+                        <img src={img5} alt="Shoes" className="rounded-xl  h-60" />
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Toy Truck</h2>
@@ -85,9 +86,9 @@ const Gallery = () => {
 
                 {/* ///////////// */}
 
-                <div className="card w-full bg-base-100 drop-shadow-2xl">
+                <div className="card w-96 bg-base-100 drop-shadow-2xl">
                     <figure className="px-10 pt-10">
-                        <img src={img6} alt="Shoes" className="rounded-xl" />
+                        <img src={img6} alt="Shoes" className="rounded-xl  h-60" />
                     </figure>
                     <div className="card-body items-center text-center">
                         <h2 className="card-title">Toy Truck</h2>
