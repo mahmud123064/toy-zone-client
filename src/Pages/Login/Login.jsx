20becc14ef0edfbfda66e8cd48d4db8e352ffe9b
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-19 16:51:49 +0600

    Banner Created

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
         <div className="hero min-h-screen  bg-base-200">
             <div className="hero-content flex-col ">
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body ">
+                    <div className="card-body p-16 ">
                         <h2 className="text-center text-3xl font-bold">Please Login</h2>
                         <form>
                             <div className="form-control">
@@ -24,11 +24,12 @@ const Login = () => {
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
-                        </form>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                            <input type="btn" value="Login" />
+
+                            <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
                         </div>
+                        </form>
+                        
                         <p>Are you new? <Link to='/register'><span className="text-red-500">Please Register</span></Link></p>
 
                         <div className="flex">
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
+import AuthProvider from './Provider/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto mb-6'>
-    <React.StrictMode>
-  <RouterProvider router={router} />
-  </React.StrictMode>,
+    <AuthProvider>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>,
+    </AuthProvider>
   </div>
 )
